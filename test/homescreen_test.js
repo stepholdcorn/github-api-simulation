b037e96d7bbbd792b44ba88e7e9f291f5ab4309c
Andy <andygnewman@gmail.com>
2015-02-05 17:46:40 +0000

    test initiation for jQuery

--- test/homescreen_test.js
+++ test/homescreen_test.js
@@ -1,5 +1,11 @@
 describe('homepage', function() {
 
+var host = 'http://localhost:9999/';
+
+before(function() {
+  casper.start(host);
+});
+
   it('should show steph\'s profile', function() {
     casper.then(function() {
       expect('body').to.include.text('stepholdcorn');
