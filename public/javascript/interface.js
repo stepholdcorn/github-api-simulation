b9430692688494a83c88274a85c6172b10f3b861
Andy <andygnewman@gmail.com>
2015-02-06 10:27:32 +0000

    add functionality to capture profile by username entry

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
     var newProfile = Mustache.render($('#profile-template').html(), user);
     $('.profile-container').append(newProfile);
   });
+
+  $('#add_profile').on('submit', function(event) {
+    event.preventDefault();
+    console.log($('#username'));
+    var newUser = $('#username').val();
+    $.get('http://api.github.com/users/' + newUser, function(user) {
+    var newProfile = Mustache.render($('#profile-template').html(), user);
+    $('.profile-container').append(newProfile);
+    });
+  });
 });
