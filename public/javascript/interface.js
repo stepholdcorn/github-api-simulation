6af917f29dd4608d53958896e2d5babb9bdcba7c
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-06 10:53:49 +0000

    Refactored

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
-  $.get('http://api.github.com/users/stepholdcorn', function(user) {
-    var newProfile = Mustache.render($('#profile-template').html(), user);
-    $('.profile-container').append(newProfile);
-  });
+  loadProfile('stepholdcorn');
 
   $('#add_profile').on('submit', function(event) {
     event.preventDefault();
     console.log($('#username'));
     var newUser = $('#username').val();
-    $.get('http://api.github.com/users/' + newUser, function(user) {
-    var newProfile = Mustache.render($('#profile-template').html(), user);
-    $('.profile-container').append(newProfile);
-    });
+    loadProfile(newUser);
   });
 });
+
+var loadProfile = function(newUser) {
+  $.get('http://api.github.com/users/' + newUser, function(user) {
+  var newProfile = Mustache.render($('#profile-template').html(), user);
+  $('.profile-container').append(newProfile);
+  });
+}
